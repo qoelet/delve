cca47855d8d1207526874c0781eef1cdb3b6fa3a
qoelet <kenny@northpole.sg>
2014-01-23 23:26:49 +0800

    time series functions

---
+++ R/timeSeries.R
@@ -0,0 +1,11 @@
+# Some functions for time series calculations
+
+dp = function(x) {
+    d <- diff(x)
+    acf(d)
+    print("Mean:")
+    print(mean(d))
+    print("Interval:")
+    print(mean(d) + c(-2,2) * (sd(d)/sqrt(length(ds))))
+    return(d)
+}
