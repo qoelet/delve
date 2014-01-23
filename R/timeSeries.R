272abd1d083e34f5b186d160d0a729aed5922d0a
qoelet <kenny@northpole.sg>
2014-01-23 23:34:40 +0800

    dp - ignore NA values

--- R/timeSeries.R
+++ R/timeSeries.R
@@ -6,6 +6,6 @@ dp = function(x) {
     print("Mean:")
     print(mean(d))
     print("Interval:")
-    print(mean(d) + c(-2,2) * (sd(d)/sqrt(length(d))))
-    return(mean(d))
+    print(mean(d, na.rm=TRUE) + c(-2,2) * (sd(d, na.rm=TRUE)/sqrt(length(d))))
+    return(mean(d, na.rm=TRUE))
 }
