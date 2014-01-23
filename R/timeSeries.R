fc2d041d2f3586c28cc1133173817fe200d43b8a
qoelet <kenny@northpole.sg>
2014-01-23 23:30:23 +0800

    Fix typo

--- R/timeSeries.R
+++ R/timeSeries.R
@@ -6,6 +6,6 @@ dp = function(x) {
     print("Mean:")
     print(mean(d))
     print("Interval:")
-    print(mean(d) + c(-2,2) * (sd(d)/sqrt(length(ds))))
+    print(mean(d) + c(-2,2) * (sd(d)/sqrt(length(d))))
     return(d)
 }
